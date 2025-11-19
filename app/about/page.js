5e658fc1e90bc9a5b4378d2a286198ec041235c4
harrycrcz <cristaldomarcelo25@gmail.com>
2025-11-19 17:41:20 -0300

    feat: create home, about, and blog pages

---
+++ app/about/page.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This is my Cognitve Science blog. Let's get nerdy</p>
+    </div>
+  );
+}
---
+++ app/blog/page.js
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <div>
+      <h1>What is what we call mind, anyway?</h1>
+      <p>Here we are going to resume the key elements to 'build a mind'. An easy-to-define concept,
+        don't you think?
+      </p>
+    </div>
+  );
+}
