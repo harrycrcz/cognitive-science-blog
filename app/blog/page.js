d216a87a5bbe28f37e991e0835d00a1a67f93cdd
harrycrcz <cristaldomarcelo25@gmail.com>
2025-11-25 12:11:03 -0300

    feat: add programmatic navigation with useRouter in About page

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,10 +1,21 @@
+'use client';
+import { useRouter } from  'next/navigation';
+
 export default function About() {
+  const router = useRouter();
+
+  const handleClick = ()=> {
+    router.push('/')
+  };
+  
   return (
     <div>
       <h1>What is what we call mind, anyway?</h1>
       <p>Here we are going to resume the key elements to 'build a mind'. An easy-to-define concept,
         don't you think?
       </p>
+      <button onClick={handleClick}>Go back</button>
+
     </div>
   );
 }
